3226ac7050e6b29aac3af638822e71e5624b469e
Jose <josevitorrodrigues17@gmail.com>
2024-09-09 20:51:12 -0300

    feat(db): add database connection

---
+++ src/db.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const dsn = process.env.DB_CONNECTION;
+
+console.log(dsn);
+
+const sequelize = new Sequelize(dsn);
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
+
+
+module.exports = sequelize
--- src/index.js
+++ src/index.js
@@ -4,4 +4,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT
 
+
+console.log(PORT)
+
 app.listen(PORT,()=> console.log(`app running on ${PORT}`))
