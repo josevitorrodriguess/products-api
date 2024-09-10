19b355b891ef22ca8e112455eeed4b0123bbd387
Jose <josevitorrodrigues17@gmail.com>
2024-09-10 10:12:52 -0300

    refactor(routes): change   route getAll and add getbyorder

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const productController = require('../controller/productController')
 
 router.post('/', productController.createProduct)
-
+router.get('/findAll', productController.getAllProducts)
+router.get('/:id', productController.getProductById)
+router.get('/', productController.getOrderedByPrice)
 
 module.exports = router
