f8e70cd81efe1c9605c8362011739fe2d5acf1a6
Jose <josevitorrodrigues17@gmail.com>
2024-09-09 23:14:45 -0300

    feat(service): add validation for name of product

---
+++ src/service/productService.js
@@ -0,0 +1,40 @@
+const productRepository = require('../repository/productRepository')
+
+class ProductService {
+
+    async createProduct(data) {
+        try {
+        
+          const existingProduct = await productRepository.productExists(data.name)
+          if (existingProduct) {
+            throw new Error('A product with this name already exists')
+          }
+        
+          
+          return await productRepository.create(data)
+        } catch (error) {
+          throw new Error(`Failed to create product: ${error.message}`)
+        }
+      }
+    
+
+  async getAllProducts() {
+    return await productRepository.getAll()
+  }
+
+
+  async getProductById(id) {
+    return await productRepository.getById(id)
+  }
+
+
+  async updateProduct(id, data) {
+    return await productRepository.update(id, data)
+  }
+
+  async deleteProduct(id) {
+    return await productRepository.delete(id)
+  }
+}
+
+module.exports = new ProductService()
