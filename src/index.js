d307581a350b06933375dfecdf07af616265c347
Jose <josevitorrodrigues17@gmail.com>
2024-09-09 22:05:50 -0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 require('dotenv').config()
 const express = require('express')
+const setup = require('./config')
 
-const app = express()
-const PORT = process.env.PORT
+const app = express();
+const PORT = process.env.PORT || 3000
 
+app.use(express.json())
 
-console.log(PORT)
+console.log('PORT:', PORT)
 
-app.listen(PORT,()=> console.log(`app running on ${PORT}`))
+setup()
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+  })
