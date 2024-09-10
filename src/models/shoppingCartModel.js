f4ec85fcb877d58d41c976fa2f859557f7bb9df8
Jose <josevitorrodrigues17@gmail.com>
2024-09-10 13:51:48 -0300

    feat(models): add model to shoppingcart

---
+++ src/models/shoppingCartModel.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize')
+const sequelize = require('../database/db')
+
+const ShoppingCart = sequelize.define('ShoppingCart', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    product_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'Products',  
+            key: 'id'          
+        },
+        onUpdate: 'CASCADE',    
+        onDelete: 'CASCADE',    
+    },
+    quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
+    }
+})
+
+module.exports = ShoppingCart
