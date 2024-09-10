870ba8b41d0d190d0cee25298f434975d97ca71d
Jose <josevitorrodrigues17@gmail.com>
2024-09-09 23:24:29 -0300

    feat(controller): add controller to get products

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -28,6 +28,16 @@ class ProductController {
         res.status(500).json({ error: error.message })
       }
     }
+
+
+    async getAllProducts(req,res){
+        try {
+            const products = await productService.getAll()
+            res.status(200).json(products)
+          } catch (error) {
+            res.status(500).json({ error: error.message })
+          }  
+        }
 }
 
 
